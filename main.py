d8d3da692b972d8101b0c0b87378b86fa84bc9b5
resist15 <sourav@aospa.co>
2024-06-12 15:27:08 +0530

    blog-backend: Finish POST req method for `/blog` endpoint
    this method will be used for adding blogs to the database
    
    Change-Id: I27a71ab7e68ee7ed3795551e374620778eb21a80

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from blog import schemas, models
-from blog.database import engine
+from blog.database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
-@app.get('/blog')
-def create(request: schemas.Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog')
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
