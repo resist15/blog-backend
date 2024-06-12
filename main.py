bc6baa2abb375f5106c6bc42a15257f0623306da
resist15 <sourav@aospa.co>
2024-06-12 15:05:08 +0530

    blog-backend: models: Create base table for blogs
    Change-Id: I836b61edb172de39aa05c894e3c2cb3b055c7482

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from blog import schemas
-
+from blog import schemas, models
+from blog.database import engine
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
 
 @app.get('/blog')
 def create(request: schemas.Blog):
