acccf69938c0bced2188b8b51995af527e9ac223
resist15 <sourav@aospa.co>
2024-06-12 17:26:58 +0530

    blog-backend: Finish `{delete,update}` methods & endpoints
    Change-Id: I7b3a86d79c9e8e0fcf820b88962b24e8e65865c5

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from blog import schemas, models
 from blog.database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 app = FastAPI()
 
@@ -16,10 +17,42 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy_blog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return f'Blog with id {id} deleted sucessfully'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    db.execute(
+        update(models.Blog)
+        .where(models.Blog.id == id)
+        .values(request.dict())
+    )
+    db.commit()
+    return 'updated sucessfully'
+
+
+@app.get('/blog')
+def all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def show_blogs(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not found')
+    return blog
