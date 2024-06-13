001fe8a14d39d9bc028f3d12703ec2fa25344feb
resist15 <sourav@aospa.co>
2024-06-13 13:04:17 +0530

    blog-backend: Define relation users->blogs
    Change-Id: I0b23df9056935462632b0b1ad36847d269daffb5

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,28 @@
 from pydantic import BaseModel
+from typing import List
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
+
+class Blog(BlogBase):
+    class Config:
+        from_attributes = True
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog]
+
+    class Config:
+        from_attributes = True
 
 
 class ShowBlog(BaseModel):
     title: str
     body: str
+    creator: ShowUser
 
     class Config:
         from_attributes = True
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from blog.database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from sqlalchemy import update
 from typing import List
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -18,16 +19,16 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED,tags=['blogs'])
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body,user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def destroy_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -37,7 +38,7 @@ def destroy_blog(id, db: Session = Depends(get_db)):
     return f'Blog with id {id} deleted sucessfully'
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
 def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -52,13 +53,13 @@ def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'Blog has been updated sucessfully'
 
 
-@app.get('/blog', response_model=List[schemas.ShowBlog])
+@app.get('/blog', response_model=List[schemas.ShowBlog],tags=['blogs'])
 def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog,tags=['blogs'])
 def show_blogs(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -66,10 +67,17 @@ def show_blogs(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post('/user')
+@app.post('/user',response_model=schemas.ShowUser,tags=['user'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}',response_model=schemas.ShowUser,tags=['user'])
+def get_user(id:int, db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'user with id {id} not found')
+    return user
